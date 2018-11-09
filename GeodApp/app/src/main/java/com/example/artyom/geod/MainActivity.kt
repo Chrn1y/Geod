74b3751e006e6d3b1cd8bfaf73eaafaf6a401c97
Chrn1y <artyom.sushkov@gmail.com>
2018-11-09 14:04:45 +0300

    add markers activity

--- GeodApp/app/src/main/java/com/example/artyom/geod/MainActivity.kt
+++ GeodApp/app/src/main/java/com/example/artyom/geod/MainActivity.kt
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.view.View
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
